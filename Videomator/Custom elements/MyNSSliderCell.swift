5a3fd77e8305c663ede5df9a6b1f2c7b46206f05
danylo <ltvnchkd@gmail.com>
2022-07-11 17:55:03 +0200

    timeline incapsulated

--- Videomator/Custom elements/MyNSSliderCell.swift
+++ Videomator/Custom elements/MyNSSliderCell.swift
@@ -6,18 +6,24 @@
 //
 
 import Cocoa
+import AVKit
+import AVFoundation
 
 class MyNSSliderCell: NSSliderCell {
     
-    var pattern = [NSImage]()
-    
-    public func setPatterns(with pattern: NSImage, requiredAmount: Int) {
-        self.pattern.append(pattern)
-        if self.pattern.count == requiredAmount {
-            self.controlView?.layoutSubtreeIfNeeded()
+    private var pattern = [NSImage]()
+    public var videoURL: URL? {
+        didSet {
+            Task(priority: .userInitiated) {
+                await self.setPreviews()
+            }
         }
     }
+    private var requiredNumberOfCaptures: CGFloat {
+        self.controlView!.frame.width / self.controlView!.frame.height
+    }
     
+    //MARK: - Inherited functions
     override func draw(withFrame cellFrame: NSRect, in controlView: NSView) {
         if self.pattern.isEmpty {
             NSColor.magenta.setStroke()
@@ -34,14 +40,7 @@ class MyNSSliderCell: NSSliderCell {
         }
         super.draw(withFrame: cellFrame, in: controlView)
     }
-    
-    private func getNextCapture(for image: NSImage, in rect: NSRect) -> NSBezierPath {
-        let path = NSBezierPath(rect: rect)
-        NSColor(patternImage: image).set()
-        path.fill()
-        return path
-    }
-    
+
     override func drawKnob(_ knobRect: NSRect) {
         let path = NSBezierPath(roundedRect: knobRect, xRadius: 4, yRadius: 4)
         NSColor.white.withAlphaComponent(0.8).set()
@@ -50,6 +49,66 @@ class MyNSSliderCell: NSSliderCell {
     
     override func drawBar(inside rect: NSRect, flipped: Bool) {
         
+    }
+    
+    //MARK: - Supporting func
+    private func setPreviews() async {
+        guard let url = videoURL, let times = self.getTimeCodes() else { return }
+        
+        let asset = AVURLAsset(url: url)
+        let generator = AVAssetImageGenerator(asset: asset)
+        generator.maximumSize = CGSize(
+            width: self.controlView!.frame.height * 2,
+            height: self.controlView!.frame.height
+        )
+        
+        generator.generateCGImagesAsynchronously(forTimes: times) { _, image, _, result, _ in
+            let newImage = NSImage(cgImage: image!, size: .zero)
+            DispatchQueue.main.async {
+                self.setPatterns(
+                    with: newImage,
+                    requiredAmount: times.count
+                )
+                print(self.pattern.count)
+            }
+        }
+    }
+    
+    private func getTimeCodes() -> [NSValue]? {
+        guard let url = self.videoURL else { return nil }
+        let asset = AVURLAsset(url: url)
+        let numberOfFrames = self.requiredNumberOfCaptures
+        let duration = CMTimeGetSeconds(asset.duration)
+        var times = [NSValue]()
+        var k: Double = 0
+        
+        if numberOfFrames < duration {
+            k = duration / numberOfFrames
+        } else {
+            k = numberOfFrames / duration
+        }
+        
+        var sum = 0.0
+        while sum < duration {
+            let time = sum
+            times.append(time as NSValue)
+            sum += k
+        }
+        return times
+    }
+    
+    public func setPatterns(with pattern: NSImage, requiredAmount: Int) {
+        self.pattern.append(pattern)
+        if self.pattern.count == requiredAmount {
+            self.controlView?.layoutSubtreeIfNeeded()
+        }
+    }
+    
+    private func getNextCapture(for image: NSImage, in rect: NSRect) -> NSBezierPath {
+        let path = NSBezierPath(rect: rect)
+        NSColor(patternImage: image).set()
+        path.fill()
+        return path
     }
     
 }
--- Videomator/MainViewController.swift
+++ Videomator/MainViewController.swift
@@ -32,58 +32,60 @@ class MainViewController: NSViewController {
         self.videoContainer.layer = self.playerLayer
         self.videoContainer.wantsLayer = true
         self.player.play()
-        
-        Task {
-            await self.getPreviews(url: MainViewController.url)
-        }
+        self.sliderCell.videoURL = MainViewController.url
+        self.sliderCell.isEnabled = false
+//        Task {
+//            await self.getPreviews(url: MainViewController.url)
+//        }
     }
     
-    private func getPreviews(url: URL) async {
-        let asset = AVURLAsset(url: url)
-        let generator = AVAssetImageGenerator(asset: asset)
-        generator.maximumSize = CGSize(
-            width: self.sliderCell.controlView!.frame.height * 2,
-            height: self.sliderCell.controlView!.frame.height
-        )
-        
-        let times = self.getTimeCodes()
-        
-        generator.generateCGImagesAsynchronously(forTimes: times) { _, image, _, result, _ in
-            let newImage = NSImage(cgImage: image!, size: .zero)
-            DispatchQueue.main.async {
-                self.sliderCell.setPatterns(
-                    with: newImage,
-                    requiredAmount: times.count
-                )
-            }
-        }
-    }
+//    private func getPreviews(url: URL) async {
+//        let asset = AVURLAsset(url: url)
+//        let generator = AVAssetImageGenerator(asset: asset)
+//        generator.maximumSize = CGSize(
+//            width: self.sliderCell.controlView!.frame.height * 2,
+//            height: self.sliderCell.controlView!.frame.height
+//        )
+//        
+//        let times = self.getTimeCodes()
+//        
+//        generator.generateCGImagesAsynchronously(forTimes: times) { _, image, _, result, _ in
+//            let newImage = NSImage(cgImage: image!, size: .zero)
+//            DispatchQueue.main.async {
+//                self.sliderCell.setPatterns(
+//                    with: newImage,
+//                    requiredAmount: times.count
+//                )
+//                print(self.sliderCell.pattern.count)
+//            }
+//        }
+//    }
     
-    private func getTimeCodes() -> [NSValue] {
-        let asset = AVURLAsset(url: MainViewController.url)
-        let numberOfFrames = self.sliderCell.controlView!.frame.width / self.sliderCell.controlView!.frame.height
-        let duration = CMTimeGetSeconds(asset.duration)
-        var times = [NSValue]()
-        var k: Double = 0
-        
-        if numberOfFrames < duration {
-            k = duration / numberOfFrames
-        } else {
-            k = numberOfFrames / duration
-        }
-        
-        var sum = 0.0
-        while sum < duration {
-            let time = sum
-            times.append(time as NSValue)
-            sum += k
-        }
-        return times
-    }
+//    private func getTimeCodes() -> [NSValue] {
+//        let asset = AVURLAsset(url: MainViewController.url)
+//        let numberOfFrames = self.sliderCell.controlView!.frame.width / self.sliderCell.controlView!.frame.height
+//        let duration = CMTimeGetSeconds(asset.duration)
+//        var times = [NSValue]()
+//        var k: Double = 0
+//        
+//        if numberOfFrames < duration {
+//            k = duration / numberOfFrames
+//        } else {
+//            k = numberOfFrames / duration
+//        }
+//        
+//        var sum = 0.0
+//        while sum < duration {
+//            let time = sum
+//            times.append(time as NSValue)
+//            sum += k
+//        }
+//        return times
+//    }
     
     override func viewDidLayout() {
         super.viewDidLayout()
-        //        self.playerLayer.frame = self.videoContainer.frame
+        self.playerLayer.frame = self.videoContainer.frame
     }
     
     @IBAction private func openVideo(_ sender: Any) {
@@ -93,25 +95,13 @@ class MainViewController: NSViewController {
             if result == .OK, let url = openPanel.url {
                 guard let pbPath = ResourcesManager.shared.pbURL?.path,
                       let tessPath = ResourcesManager.shared.tessURL?.path,
-                      let imgPath = ResourcesManager.shared.testImgURL?.path else { return }
+                      let _ = ResourcesManager.shared.testImgURL?.path else { return }
                 self.blurer = BlurerWrapper(pbPath, tessPath)
                 self.blurer?.load(url.path)
                 self.blurer?.detect(0)
                 self.imageView.image = self.blurer?.buffer()
             }
         }
-    }
-    
-    func imagePreview(from moviePath: URL, in seconds: Double) -> NSImage? {
-        let timestamp = CMTime(seconds: seconds, preferredTimescale: 60)
-        let asset = AVURLAsset(url: moviePath)
-        let generator = AVAssetImageGenerator(asset: asset)
-        generator.appliesPreferredTrackTransform = true
-        
-        guard let imageRef = try? generator.copyCGImage(at: timestamp, actualTime: nil) else {
-            return nil
-        }
-        return NSImage(cgImage: imageRef, size: .zero)
     }
     
 }
